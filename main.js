69ec0cad4684fa693d2bea0506473eb9b6412dc9
yujiseok <koesijuy@urbanisom.com>
2022-03-25 11:05:37 +0900

    second commit

--- main.js
+++ main.js
@@ -26,3 +26,21 @@ function onsubmit(e) {
     emailInput.value = "";
   }
 }
+
+/////////
+
+const box = document.getElementById("box");
+const output = document.getElementById("output");
+
+box.addEventListener("mousemove", runEvent);
+
+function runEvent(e) {
+  // console.log(`event type: ${e.type}`);
+  let a = `${e.offsetX}`;
+  let b = `${e.offsetY}`;
+
+  output.innerHTML = `<h3>offsetX: ${e.offsetX}</h3> <h3>offsetY: ${e.offsetY}</h3>`;
+
+  console.log(a, b);
+  box.style.backgroundColor = "rgb(a, b, 40)"; // 이거 왜 안되는지 알아보기
+}
